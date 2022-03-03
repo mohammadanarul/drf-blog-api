e8fb50bec9d4e5ce5e83c6e30f7f89d6883c993c
mohammadanarul <mdanarul7075@gmail.com>
2022-03-03 16:25:22 +0600

    BD_PASSWORD not found bug fix.

--- blogapi/settings/development.py
+++ blogapi/settings/development.py
@@ -5,9 +5,9 @@ DATABASES = {
         'ENGINE': config('DB_ENGINE'),
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
-        'PASSWORD': config('BD_PASSWORD'),
-        'HOST': config('BD_HOST'),
-        'PORT': config('BD_PORT'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
