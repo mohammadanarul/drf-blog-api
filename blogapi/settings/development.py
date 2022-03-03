2d3d6ab7c173fc02c2018436a42790225432d72e
mohammadanarul <mdanarul7075@gmail.com>
2022-03-03 17:42:18 +0600

    Final bug fix.

--- blogapi/settings/development.py
+++ blogapi/settings/development.py
@@ -13,9 +13,9 @@ DATABASES = {
 
 # SMTP services inherit
 EMAIL_HOST = config('EMAIL_HOST', default='localhost')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default=25, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
 EMAIL_BACKEND = config('EMAIL_BACKEND', default='')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='')
