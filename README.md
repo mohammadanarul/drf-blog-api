0d5a7192f23f1bb712f7d2f9c8e28d795111ac85
mohammadanarul <mdanarul7075@gmail.com>
2022-03-03 14:26:04 +0600

    project initial setup.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # drf-blog-api
+
+'''
+deactivate
+'''
+
+### Project Setup
+
+    # git clone https://github.com/mohammadanarul/drf-blog-api.git
+    # cd drf-blog-api
+    # venv\Scripts\activate
---
+++ blogapi/settings/development.py
@@ -0,0 +1,30 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': config('BD_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('BD_PASSWORD'),
+        'HOST': config('BD_HOST'),
+        'PORT': config('BD_PORT'),
+    }
+}
+
+# SMTP services inherit
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "http://127.0.0.1:9000",
+]
